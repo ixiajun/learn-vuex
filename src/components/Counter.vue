cd785f74c60ba5c5a4ad76a011cdad7f0e660bb3
xiajun1996 <aixiajun@vip.qq.com>
2017-03-21 14:20:15 +0800

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app">
+    Counter is {{ $store.state.count }}
+    <button @click="increment">+5</button>
+    <button @click="decrement">-3</button>
+  </div>
+
+</template>
+
+<script>
+  import {  mapActions } from 'vuex'
+  export default {
+//    computed: mapGetters([
+//
+//    ]),
+    methods: mapActions([
+        'increment',
+        'decrement'
+    ])
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
